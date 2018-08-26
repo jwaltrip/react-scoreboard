f2b5935d4c20c8fa3f2bc214c66fabdf0abed16e
jwaltrip <jake.waltrip@gmail.com>
2018-08-26 12:18:14 -0500

    initial commit, porting react scoreboard from prev react vs

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+/*
+List of components used:
+
+App - stateful
+Header - stateless
+Stats - stateless
+Player - stateless
+Counter - stateless
+AddPlayterForm - stateful
+
+ */
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
