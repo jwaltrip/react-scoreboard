07c2ef39adcf612b6c048254e921ac1e9389c4b7
jwaltrip <jake.waltrip@gmail.com>
2018-08-26 12:50:58 -0500

    converted Stats component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-// need to import Stats component
+import Stats from './Stats';
 
 // Header component
 const Header = (props) => {
---
+++ src/components/Stats.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// Stats component
+const Stats = (props) => {
+  // calculate total num of players and sum their point total
+  const totalPlayers = props.players.length;
+  const totalPoints = props.players.reduce((total, player) => {
+    return total + player.score;
+  }, 0);
+
+  return (
+    <table className="stats">
+      <tbody>
+      <tr>
+        <td>Players:</td>
+        <td>{totalPlayers}</td>
+      </tr>
+      <tr>
+        <td>Total Points:</td>
+        <td>{totalPoints}</td>
+      </tr>
+      </tbody>
+    </table>
+  );
+};
+
+Stats.propTypes = {
+  players: React.PropTypes.array.isRequired
+};
+
+export default Stats;
